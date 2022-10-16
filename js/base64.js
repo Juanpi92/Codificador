f6f77fe3059272378552535f07bb8589ee1cf38c
Juanpi92 <juan.urgelles92@gmail.com>
2022-10-16 01:29:18 -0300

    retocando

--- js/base64.js
+++ js/base64.js
@@ -2,6 +2,7 @@ export function Codificar_base64(texto) {
   try {
     return window.btoa(unescape(encodeURIComponent(texto)));
   } catch (error) {
+    document.querySelector("#texto2").classList.add("error_cadena");
     return "No se pudo codificar, introduzca una cadena valida";
   }
 }
@@ -10,6 +11,7 @@ export function Descodificar_base64(texto) {
   try {
     return decodeURIComponent(escape(window.atob(texto)));
   } catch (error) {
+    document.querySelector("#texto2").classList.add("error_cadena");
     return "No se pudo descodificar, introduzca una cadena valida";
   }
 }
--- js/codificador.js
+++ js/codificador.js
@@ -2,12 +2,17 @@ import { Codificar_base64, Descodificar_base64 } from "./base64.js";
 import { Codificar_Cesar, Descodificar_Cesar } from "./cifrado_cesar.js";
 
 const $formulario = document.querySelector(".formulario");
+const $texto = document.querySelector("#texto");
 $formulario.reset(); //inicializo todo
 
 const $rango_cesar = document.querySelector("#rango_cesar");
 const $rango_cesar_label = document.querySelector("#rango_cesar_label");
 const $tipo_codigo = document.querySelector("#tipo_codigo");
 $rango_cesar_label.textContent = $rango_cesar.value; //Al inicio donde debe estar
+
+//Evento validity
+/*$tipo_codigo.setCustomValidity("Debe escoger tipo de Codigo");
+$texto.setCustomValidity("Debe introducir un texto a codificar");*/
 
 //Cambiar el valor del label al cambiar el rango
 $rango_cesar.addEventListener("change", () => {
@@ -16,19 +21,23 @@ $rango_cesar.addEventListener("change", () => {
 
 //Aparecer o desaparecer el rango en funcion de lo escogido
 $tipo_codigo.addEventListener("change", () => {
+  $tipo_codigo.classList.remove("error_cadena");
   if ($tipo_codigo.value === "cesar") {
     $rango_cesar.classList.remove("none");
     $rango_cesar_label.classList.remove("none");
-  } else {
+  } else if ($tipo_codigo.value === "base64") {
     $rango_cesar.classList.add("none");
     $rango_cesar_label.classList.add("none");
+  } else {
+    /*$tipo_codigo.classList.add("error_cadena");*/
   }
 });
 
 //Boton traducir ejecucion
 document.addEventListener("submit", (event) => {
+  document.querySelector("#texto2").classList.remove("error_cadena");
   event.preventDefault(); //prevengo el envio del formulario
-  let texto = document.querySelector("#texto").value; //recojo el texto
+  let texto = $texto.value; //recojo el texto
   let $texto2 = document.querySelector("#texto2"); //Escojo el 2do textArea
   let indice_cesar = parseInt($rango_cesar.value); //recojo el indice
   let accion = document.querySelector('input[name="codificar"]:checked').value; //recojo si es cod o descod
